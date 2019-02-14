fbe1a1dd4d9a131cbbead712222faf893ad71ace
kanithanew <kanithanew@gmail.com>
2019-02-14 15:07:40 +0700

    commit;

--- src/main/java/calllog/webservices/calllog_ws/homepage/HomePage_Repository.java
+++ src/main/java/calllog/webservices/calllog_ws/homepage/HomePage_Repository.java
@@ -22,7 +22,8 @@ public interface HomePage_Repository extends JpaRepository<HomePage, Long>, JpaS
   String GET_USER_PROFILE = "SELECT * FROM gfmis_staff a LEFT JOIN gfmis_m_callposition b ON a.callposition = b.callpositionid WHERE a.calllogin = ?1";
 
   String GET_HOMEPAGE = "SELECT alldate.* FROM (select d.callid,d.touser,d.fromuser,g.modulename,g.submodulename,d.payunit,d.calldate,e.callusername,d.logprocess,d.status  FROM ( select a.callid,a.callmodule,a.callsubmodule,a.payunit,a.calldate,b.touser,b.FROMuser,b.logprocess , "+
-       "CASE   "+
+       "c.listcallid,CASE   "+
+       " WHEN  a.callstatus ='CreateCall' THEN'OpenCall' "+
        "WHEN  a.helpdeskOwner is null and (b.touser='Update' or b.touser='Create' or  b.touser='Edit' ) THEN'OpenCall'  "+
        "WHEN  b.touser ='Helpdesk' THEN 'NewCall'   WHEN  b.touser =?1 THEN 'MyInbox'  "+
        "WHEN  b.FROMuser =?1  and (b.touser='Update' or b.touser='Create' or  b.touser='Edit' )  THEN 'MyInbox'  "+
@@ -31,8 +32,8 @@ public interface HomePage_Repository extends JpaRepository<HomePage, Long>, JpaS
        "END as reppersON   FROM gfmis_calllog a   LEFT JOIN callpending b ON a.callid=b.callid   "+
        "LEFT JOIN (select DISTINCT(callid) as listcallid,'ListCall' as   "+
        "TempField FROM callpending_process where FROMuser =?1 or touser=?1 )c   ON a.callid=c.listcallid   "+
-       "where (c.listcallid is not null)  )d LEFT JOIN gfmis_staff e ON d.reppersON=e.calllogin  "+
-       "LEFT JOIN module_enable g ON d.callmodule=g.moduleid and d.callsubmodule=g.submoduleid order by d.callid DESC )alldate order by alldate.callid  ";
+       ")d LEFT JOIN gfmis_staff e ON d.reppersON=e.calllogin  "+
+       "LEFT JOIN module_enable g ON d.callmodule=g.moduleid and d.callsubmodule=g.submoduleid  where d.listcallid is not null or status ='NewCall' order by d.callid DESC )alldate order by alldate.callid  ";
 
 
        
